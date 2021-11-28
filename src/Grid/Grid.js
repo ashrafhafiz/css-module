ff91089f8b39d6977f1ec836dd293272ee44db4e
Ashraf Hafiz <ashraf.hafiz@yahoo.com>
2021-11-28 13:58:25 +0200

    2nd commit

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -3,11 +3,13 @@ import styles from "./Grid.module.css";
 import GridItem from "./GridItem";
 
 export default class Grid extends Component {
+  renderFacts() {
+    return this.props.facts.map((item) => (
+      <GridItem key={item.id} fact={item} />
+    ));
+  }
+
   render() {
-    return (
-      <div>
-        <GridItem />
-      </div>
-    );
+    return <section className={styles.grid}>{this.renderFacts()}</section>;
   }
 }
--- src/Grid/GridItem.js
+++ src/Grid/GridItem.js
@@ -3,6 +3,11 @@ import styles from "./GridItem.module.css";
 
 export default class GridItem extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className={styles.card}>
+        <h2 className={styles.title}>{this.props.fact.title}</h2>
+        <article className={styles.fact}>{this.props.fact.fact}</article>
+      </div>
+    );
   }
 }
